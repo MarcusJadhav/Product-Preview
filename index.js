021211a2e3ef85483ca95c1e587055e9a8d1a332
MarcusJadhav <marcusjadhav@gmail.com>
2024-08-08 17:29:28 +0530

    code optimized

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-function changeImg (num) {
+function changeImg (image) {
     const currentImg = document.getElementById("main-img")
-
-    if(num == 1)
-    {
-        currentImg.src="./images/1.jpg"
-    }
-    else if (num == 2) {
-        currentImg.src="./images/2.jpg"
-    }
-    else if (num == 3) {
-        currentImg.src="./images/3.jpg"
-    }
-    else if (num == 4) {
-        currentImg.src="./images/4.jpg"
-    }
-    else if (num == 5) {
-        currentImg.src="./images/5.jpg"
-    }
+    currentImg.src= image 
 }
